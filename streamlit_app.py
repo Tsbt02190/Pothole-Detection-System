22bc6e1ad47c38f89e280eb8beb8e384c17fec92
Tsabita Amnan Ghofar <136437283+Tsbt02190@users.noreply.github.com>
2024-06-08 14:53:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 # Fungsi untuk memuat model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model_path = "fourth/fourlayerDG.h5"
+    model_path = "fourth/fourlayerDG2.h5"
     model = tf.keras.models.load_model(model_path)
     return model
 
